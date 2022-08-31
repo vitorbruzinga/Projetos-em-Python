74409e3082ea0fbde90bcbc116231647cfd942be
Vitor Bruzinga <vitorbruzinga@gmail.com>
2022-08-31 10:24:41 -0300

    Add files via upload
    Lista 2 - Paradigmas em Python

---
+++ questao8.py
@@ -0,0 +1,10 @@
+print(
+    "\nDigite um numero: ")
+num1 = input()
+
+if (float(num1) > 10):
+    print(
+        "\nSeu numero e maior que 10! ")
+else:
+    print(
+        "\nObrigado, seu numero foi lido! ")
